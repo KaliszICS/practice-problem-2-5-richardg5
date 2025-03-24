ef218e7f1c5b958bb876c6ccfe2bcde4fe8ddaa2
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-03-24 19:03:14 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,13 @@ public class PracticeProblem {
 
 	}
 
-
+	public static void insertionSort(char[] characters) {
+		for (int i = 1; i < characters.length; i++) {
+			char temp = characters[i];
+			for (int e = i - 1; e >= 0 && characters[e] > temp; e--) {
+				characters[e + 1] = characters[e];
+				characters[e] = temp;
+			}
+		}
+	}
 }
